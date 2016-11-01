9f723f2114a374e5b11c3e8aea0e595cb7f804f1
wenxinsir <1847645807@qq.com>
2016-11-01 16:14:54 +0800

    添加跳转和log的工具类

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/utils/MFGT.java
@@ -0,0 +1,47 @@
+package cn.ucai.superwechat.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import com.baidu.platform.comapi.map.I;
+import com.baidu.platform.comapi.map.L;
+
+import java.util.ArrayList;
+import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.ui.LoginActivity;
+import cn.ucai.superwechat.ui.MainActivity;
+import cn.ucai.superwechat.ui.RegisterActivity;
+import cn.ucai.superwechat.ui.UserProfileActivity;
+
+
+public class MFGT {
+    public static void finish(Activity activity){
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity context,Class<?> cls){
+        Intent intent = new Intent();
+        intent.setClass(context,cls);
+        startActivity(context,intent);
+    }
+
+    public static void startActivity(Context context,Intent intent){
+       context.startActivity(intent);
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in,R.anim.push_bottom_out);
+    }
+
+    public static void startActivityForResult(Activity context,Intent intent,int requestCode){
+        context.startActivityForResult(intent,requestCode);
+        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_bottom_out);
+    }
+//    登录
+    public static void gotoLogin(Activity context){
+        startActivity(context,LoginActivity.class);
+    }
+//    注册
+    public static void gotoRegister(Activity context){
+        startActivity(context,RegisterActivity.class);
+    }
+}
