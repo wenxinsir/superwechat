d32854dee0d79ec95951f6dae541b3d85e7ffd11
wenxinsir <1847645807@qq.com>
2016-11-05 13:26:00 +0800

    修改个人资料的显示（补）

--- easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
+++ easeui/src/com/hyphenate/easeui/utils/EaseUserUtils.java
@@ -122,7 +122,12 @@ public class EaseUserUtils {
         setAppUserName("微信号 : ",username,textView);
     }
 
-    private static void setAppUserName(String suffix, String username, TextView textView) {
+    public static void setCurrentAppUserName(TextView textView) {
+        String username = EMClient.getInstance().getCurrentUser();
+        setAppUserName("",username,textView);
+    }
+
+    public static void setAppUserName(String suffix, String username, TextView textView) {
         textView.setText(suffix + username);
     }
 }
