f22f9c19c8263759ff4a7abf2ce43e557f1a96d3
wenxinsir <1847645807@qq.com>
2016-11-09 18:18:44 +0800

    删除联系人后刷新消息列表

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/MainActivity.java
@@ -57,6 +57,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.superwechat.Constant;
+import cn.ucai.superwechat.I;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.SuperWeChatHelper;
 import cn.ucai.superwechat.adapter.MainTabAdpter;
@@ -339,17 +340,16 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
             public void onReceive(Context context, Intent intent) {
                 updateUnreadLabel();
                 updateUnreadAddressLable();
-                if (currentTabIndex == 0) {
+    //            if (currentTabIndex == 0) {
                     // refresh conversation list
                     if (conversationListFragment != null) {
                         conversationListFragment.refresh();
                     }
-                } else
-                    if (currentTabIndex == 1) {
+    //            } elseif (currentTabIndex == 1) {
                     if(contactListFragment != null) {
                         contactListFragment.refresh();
                     }
-                }
+     //           }
                 String action = intent.getAction();
                 if (action.equals(Constant.ACTION_GROUP_CHANAGED)) {
                     if (EaseCommonUtils.getTopActivity(MainActivity.this).equals(GroupsActivity.class.getName())) {
@@ -522,6 +522,12 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
             updateUnreadAddressLable();
         }
 
+        boolean extra = getIntent().getBooleanExtra(I.ACTION_BACK_CONVERSATION, false);
+        L.e(TAG,"extra="+extra);
+        if(extra){
+            mLayoutTabhost.setChecked(0);
+        }
+
         // unregister this event listener when this activity enters the
         // background
         SuperWeChatHelper sdkHelper = SuperWeChatHelper.getInstance();
@@ -634,6 +640,12 @@ public class MainActivity extends BaseActivity implements DMTabHost.OnCheckedCha
             showConflictDialog();
         } else if (intent.getBooleanExtra(Constant.ACCOUNT_REMOVED, false) && !isAccountRemovedDialogShow) {
             showAccountRemovedDialog();
+        }
+
+    boolean isBack = intent.getBooleanExtra(I.ACTION_BACK_CONVERSATION, false);
+    L.e(TAG,"isBack="+isBack);
+    if(isBack) {
+        mLayoutTabhost.setChecked(0);
         }
     }
 
