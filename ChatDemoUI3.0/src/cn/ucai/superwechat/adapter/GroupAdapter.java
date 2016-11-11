3a9f7e235db14a55fdb1e8beb73f77d91931eb45
wenxinsir <1847645807@qq.com>
2016-11-11 18:26:51 +0800

    显示群头像

--- ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/GroupAdapter.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/adapter/GroupAdapter.java
@@ -27,6 +27,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.hyphenate.chat.EMGroup;
+import com.hyphenate.easeui.utils.EaseUserUtils;
 
 import java.util.List;
 
@@ -37,9 +38,11 @@ public class GroupAdapter extends ArrayAdapter<EMGroup> {
 	private LayoutInflater inflater;
 	private String newGroup;
 	private String addPublicGroup;
+	Context mContext;
 
 	public GroupAdapter(Context context, int res, List<EMGroup> groups) {
 		super(context, res, groups);
+		mContext = context;
 		this.inflater = LayoutInflater.from(context);
 		newGroup = context.getResources().getString(R.string.The_new_group_chat);
 		addPublicGroup = context.getResources().getString(R.string.add_public_group_chat);
@@ -112,6 +115,9 @@ public class GroupAdapter extends ArrayAdapter<EMGroup> {
 				convertView = inflater.inflate(R.layout.em_row_group, parent, false);
 			}
 			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position - 3).getGroupName());
+
+			EaseUserUtils.setAppGroupAvatar(mContet,getItem(position - 3).getGroupId(),
+					(ImageView) convertView.findViewById(R.id.avatar));
 
 		}
 
